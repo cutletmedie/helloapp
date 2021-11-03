48879099e47a11d886d408f9292a3bc1d08700c8
black <black@example.com>
2021-11-03 19:10:45 +0500

    fixes

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -17,6 +17,8 @@ namespace HelloApp
         static void Main(string[] args)
         {
             Console.WriteLine("hello everyone");
+            GreetWhite();
+            GreetBlack();
         }
     }
 }
